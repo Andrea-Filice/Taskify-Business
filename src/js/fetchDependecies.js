c304b6ef2382e13057d0185dac3af0b57f819660
Andrea Filice <help.playepik@gmail.com>
2025-05-03 12:30:37 +0200

    Update fetchDependecies.js

--- src/js/fetchDependecies.js
+++ src/js/fetchDependecies.js
@@ -1,5 +1,5 @@
 try{
-    fetch('https://play-epik-incorporation.netlify.app/dependecies/dependecies.json')
+    fetch('https://playepikservercontents.netlify.app/dependecies/dependecies.json')
     .then(response => response.json())
           .then(data => {
             const version = data.versionTaskify;
