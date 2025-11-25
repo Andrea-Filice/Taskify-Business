abbc89db3ac396499b53c063d4942a2decb53834
Andrea-Filice <filiceandrea8@gmail.com>
2025-11-25 22:14:40 +0100

    fix: fixing icon
    * fixed the "Edit PopUp" icon for Windows

--- config.js
+++ config.js
@@ -569,6 +569,7 @@ function createInputPopUp() {
       contextIsolation: false,
       webSecurity: true
     },
+    icon: 'src/assets/icon.ico'
   })
   inputWindow.setMenu(null)
   inputWindow.loadFile('src/popUp.html')
